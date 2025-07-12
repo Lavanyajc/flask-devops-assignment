13f249bf3ae874bef74c18693fa915099c03fec1
LAVANYA J C <139207138+Lavanyajc@users.noreply.github.com>
2025-07-12 15:49:45 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from JC's Flask app deployed via GitHub Actions, ECR, and EC2!"
+    return "Hii from JC's Flask app deployed via GitHub Actions, ECR, and EC2!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
